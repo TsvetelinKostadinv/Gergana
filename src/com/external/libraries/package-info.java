2581080869fe8a9cb4ec4a659bda75a7c408ce35
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2018-04-22 19:27:28 +0300

    Added external libraries + info file

---
+++ src/com/external/libraries/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ *<h1>This foulder contains the external libraries used within this project</h1>
+ *
+ * @author Tsvetelin
+ *
+ */
+package com.external.libraries;
---
+++ src/com/external/licenses/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package is for the licenses of the external libraries
+ * @author Tsvetelin
+ *
+ */
+package com.external.licenses;
